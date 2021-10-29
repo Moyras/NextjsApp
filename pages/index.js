b44aa5f5d6cec439f62dd6a8cb254411cbe17f90
ManuelMorion <75379455+Moyras@users.noreply.github.com>
2021-10-29 14:07:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
 import axios from "axios";
 import CourseCard from "../components/cards/CourseCard";
 
-const Index = () => {
+const Index = ({ courses }) => {
 
   return (
     <>
       <h1 className="square jumbotron text-center bg-primary">
         Online Education Marketplace
       </h1>
-      
+      <div className="container-fluid">
+        <div className="row">
+          {courses.map((course) => (
+            <div key={course._id} className="col-md-4">
+              <CourseCard course={course} />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
 
-
+export async function getServerSideProps() {
+  const { data } = await axios.get(`https://http://138.197.183.149/api/courses`);
+  return ({
+    props: { courses: data },
+  });
+}
 
 export default Index;
